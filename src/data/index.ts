639fd09e2985fa958ad950ecab7236beca510e71
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-10-01 16:57:10 +0200

    :rocket: deploy stuff

--- src/data/index.ts
+++ src/data/index.ts
@@ -18,11 +18,41 @@ export const projects: Project[] = [
     id: 5,
     name: "MetaTags for Frameworks",
     slug: "metatags-for-frameworks",
-    date: "7 avril 2024",
+    date: "Avril 2024",
     description:
       "L'outil pour ajouter des métadonnées à votre framework. Gérez sans effort le référencement, le partage social et les intégrations de vos applications Web.",
     cover: "/images/metatags.webp",
     category: ProjectCategory.Tools,
   },
-  
+  {
+    id: 10,
+    name: "baudelAIre",
+    slug: "baudelaire",
+    date: "Janvier 2024",
+    description:
+      "Générateur de poème avec IA (Mistral).",
+    cover: "/images/baudelaire.webp",
+    category: ProjectCategory.Tools,
+  },
+  {
+    id: 13,
+    name: "BEL",
+    slug: "bel",
+    date: "Décembre 2023",
+    description:
+      "Site vitrine pour l'entreprise Bureau d'Études Legavre.",
+    cover: "/images/bel.png",
+    category: ProjectCategory.Webdesign,
+  },
+  {
+    id: 14,
+    name: "zLawyer",
+    slug: "zlawyer",
+    date: "Décembre 2022",
+    description:
+      "Site vitrine pour l'entreprise zLawyer, logiciel de facturation de cabinets d'avocats.",
+    cover: "/images/zlawyer.webp",
+    category: ProjectCategory.Webdesign,
+  },
+
 ];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        black: '#111113',
+        'black-light': '#18191b',
+        // GREEN
+        'dark-green-light': '#6fd392',
+        'dark-green-dark': '#1a2c24',
+        'green-light': '#e9f6ec',
+        'green-dark': '#307f52',
+        // RED
+        'dark-red-light': '#ff8fa8',
+        'dark-red-dark': '#341726',
+        'red-light': '#fceaf0',
+        'red-dark': '#ba3263',
+        // BLUE
+        'blue-default': '#2F80EC',
+        'dark-blue-light': '#a1b0ff',
+        'dark-blue-dark': '#1b2448',
+        'blue-light': '#DBEAFE75',
+        'blue-dark': '#415ac1',
+      },
+    },
 	},
 	plugins: [],
 }
