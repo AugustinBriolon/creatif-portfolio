f6f7113efc7a4573537f50dfd124647417e0c3fc
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-10-02 17:10:43 +0200

    :rocket: deploy stuff

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,11 @@
 export enum ProjectCategory {
-  Tools = "tools",
-  Webdesign = "webdesign",
+  Tools = 'tools',
+  Webdesign = 'webdesign',
+}
+
+interface Team {
+  name: string;
+  link: string;
 }
 
 export interface Project {
@@ -9,50 +14,108 @@ export interface Project {
   slug: string;
   date: string;
   description: string;
+  team?: Team[];
   cover: string;
+  url: string;
   category: ProjectCategory;
 }
 
 export const projects: Project[] = [
   {
-    id: 5,
-    name: "MetaTags for Frameworks",
-    slug: "metatags-for-frameworks",
-    date: "Avril 2024",
+    id: 6,
+    name: 'MetaTags for Frameworks',
+    slug: 'metatags-for-frameworks',
+    date: 'Avril 2024',
     description:
       "L'outil pour ajouter des métadonnées à votre framework. Gérez sans effort le référencement, le partage social et les intégrations de vos applications Web.",
-    cover: "/images/metatags.webp",
+
+    cover: '/images/metatags.webp',
+    url: 'https://metatagframework.august1.dev/',
     category: ProjectCategory.Tools,
   },
   {
-    id: 10,
-    name: "baudelAIre",
-    slug: "baudelaire",
-    date: "Janvier 2024",
-    description:
-      "Générateur de poème avec IA (Mistral).",
-    cover: "/images/baudelaire.webp",
+    id: 5,
+    name: 'baudelAIre',
+    slug: 'baudelaire',
+    date: 'Janvier 2024',
+    description: 'Générateur de poème avec IA (Mistral).',
+    team: [
+      {
+        name: 'Nicolas Bordeaux',
+        link: 'https://www.nicolasbordeaux.xyz/',
+      },
+      {
+        name: 'Thomas Aubert',
+        link: 'https://github.com/N0rooo',
+      },
+    ],
+    cover: '/images/baudelaire.webp',
+    url: 'https://baudel-ai-re.vercel.app/',
     category: ProjectCategory.Tools,
   },
   {
-    id: 13,
-    name: "BEL",
-    slug: "bel",
-    date: "Décembre 2023",
+    id: 4,
+    name: 'Find Your Meal',
+    slug: 'findyourmeal',
+    date: 'Janvier 2024',
     description:
-      "Site vitrine pour l'entreprise Bureau d'Études Legavre.",
-    cover: "/images/bel.png",
+      "Des aliments mais pas d'idée de repas ? Ce site est la solution !",
+    cover: '/images/findyourmeal.webp',
+    url: 'https://findyourmeal.august1.dev/',
+    category: ProjectCategory.Tools,
+  },
+  {
+    id: 3,
+    name: 'BEL',
+    slug: 'bel',
+    date: 'Décembre 2023',
+    description: "Site vitrine pour l'entreprise Bureau d'Études Legavre.",
+    team: [
+      {
+        name: 'Matteo Courquin',
+        link: 'https://matteo.courqu.in/',
+      },
+    ],
+    cover: '/images/bel.webp',
+    url: 'https://www.be-legavre.com/',
     category: ProjectCategory.Webdesign,
   },
   {
-    id: 14,
-    name: "zLawyer",
-    slug: "zlawyer",
-    date: "Décembre 2022",
+    id: 2,
+    name: 'ToolsBox 4 Dev',
+    slug: 'toolsbox4dev',
+    date: 'Février 2023',
+    description:
+      "Site regroupant plusieurs outils utiles pour les développeurs.",
+    team: [
+      {
+        name: 'Matteo Courquin',
+        link: 'https://matteo.courqu.in/',
+      },
+      {
+        name: 'Adel Khiter',
+        link: 'https://github.com/Adelktr',
+      },
+    ],
+    cover: '/images/toolsfordev.webp',
+    url: 'https://tool4dev.vercel.app/',
+    category: ProjectCategory.Tools,
+  },
+  {
+    id: 1,
+    name: 'zLawyer',
+    slug: 'zlawyer',
+    date: 'Décembre 2022',
     description:
       "Site vitrine pour l'entreprise zLawyer, logiciel de facturation de cabinets d'avocats.",
-    cover: "/images/zlawyer.webp",
+    team: [
+      {
+        name: 'Matteo Courquin',
+        link: 'https://matteo.courqu.in/',
+      },
+    ],
+    cover: '/images/zlawyer.webp',
+    url: 'https://www.zlawyer.fr/logiciel-avocats/',
     category: ProjectCategory.Webdesign,
   },
-
 ];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,25 +4,29 @@ export default {
 	theme: {
 		extend: {
       colors: {
+        btn: "#0000000d",
         black: '#111113',
         'black-light': '#18191b',
         // GREEN
-        'dark-green-light': '#6fd392',
-        'dark-green-dark': '#1a2c24',
         'green-light': '#e9f6ec',
         'green-dark': '#307f52',
         // RED
-        'dark-red-light': '#ff8fa8',
-        'dark-red-dark': '#341726',
         'red-light': '#fceaf0',
         'red-dark': '#ba3263',
         // BLUE
         'blue-default': '#2F80EC',
-        'dark-blue-light': '#a1b0ff',
-        'dark-blue-dark': '#1b2448',
         'blue-light': '#DBEAFE75',
         'blue-dark': '#415ac1',
       },
+      backgroundImage: {
+        'svgBg': "url('/background.svg')",
+      },
+      gap: {
+        projects: "clamp(1.5rem, 1.02rem + 1.85vw, 2.5rem)"
+      },
+      cursor: {
+        'link': "url('/icons/arrow-up.svg') 15 2, pointer;",
+      }
     },
 	},
 	plugins: [],
